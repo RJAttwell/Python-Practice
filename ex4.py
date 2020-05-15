e6feee5795b5a8a7f012a3e3616b01e96f3999ee
RJAttwell <48221630+RJAttwell@users.noreply.github.com>
2020-05-15 14:28:24 +0100

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,15 @@
+my_name = 'Zaquiorious D. Ballabeenus'
+my_age = 43
+my_height = 7 #feet
+my_weight = 384 #lbs
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'BLACK'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} feet tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"87% Bodyfat.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the time of the day.")
+
