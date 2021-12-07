a8eaf5f002b0aeae9178622b2f77fbbef11c2366
RJAttwell <48221630+RJAttwell@users.noreply.github.com>
2021-12-07 00:25:03 +0000

    Add files via upload

---
+++ VariablesInt.py
@@ -0,0 +1,9 @@
+age = 24
+age += 1
+
+#print(age)
+#print(type(age)) #Checks data type
+
+print("My age is: " + str(age)) #Convert int variable to string
+
+
---
+++ VariablesStr.py
@@ -0,0 +1,17 @@
+#Variable = Stores a value. Behaves as value it contains.
+#Can store booleans, numbers or strings
+
+#name = ("Richard")
+#print(name)
+#print("Hello "+name)
+
+#print(type(name)) #Allows you to data type of variable
+
+#Can combine variables if of the same data type
+
+first_name = ("Richard ")
+last_name = ("Attwell")
+full_name = first_name + last_name
+
+print("Hello "+full_name)
+
