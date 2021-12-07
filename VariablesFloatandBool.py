42567d62d4c5f5b7059a838892ce6e370b5b1cfc
RJAttwell <48221630+RJAttwell@users.noreply.github.com>
2021-12-07 00:40:11 +0000

    Add files via upload

---
+++ VariablesFloatandBool.py
@@ -0,0 +1,12 @@
+height = 188.5
+#print(height)
+#print(type(height))
+
+#print("My height is: "+str(height)+ "cm")
+
+
+#Boolean Values:
+human = True
+print("Are you a human: "+ str(human))
+#print(type(human))
+
